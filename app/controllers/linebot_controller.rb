9c96edc6a80a3bff279de9d060d11a3b09f9729c
shimpei1009 <gogo.ikeike.1009@icloud.com>
2020-03-19 13:49:57 +0900

    make temperature

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -36,49 +36,53 @@ class LinebotController < ApplicationController
             per06to12 = doc.elements[xpath + 'info[2]/rainfallchance/period[2]'].text
             per12to18 = doc.elements[xpath + 'info[2]/rainfallchance/period[3]'].text
             per18to24 = doc.elements[xpath + 'info[2]/rainfallchance/period[4]'].text
+            maxtemp = doc.elements[xpath + 'info[2]/temperature/range[1]'].text
+            mintemp = doc.elements[xpath + 'info[2]/temperature/range[2]'].text
             if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
+              # もし降水確率がmin_per以上の場合
               push =
-                "明日の天気だよね。\n明日は雨が降りそうだよ(>_<)\n今のところ降水確率はこんな感じだよ。\n　  6〜12時　#{per06to12}％\n　12〜18時　 #{per12to18}％\n　18〜24時　#{per18to24}％\nまた明日の朝の最新の天気予報で雨が降りそうだったら教えるね！"
+                "明日は雨が降りそう...\nでも、魚は活発になるよ！\n 最高気温 #{maxtemp}％\n 最低気温 #{mintemp}％\n 降水確率：\n 6〜12時 #{per06to12}％\n 12〜18時  #{per12to18}％\n 18〜24時 #{per18to24}％\n"
             else
               push =
-                "明日の天気？\n明日は雨が降らない予定だよ(^^)\nまた明日の朝の最新の天気予報で雨が降りそうだったら教えるね！"
+                "明日は釣り日和になるよ！\nタックルの準備だ！\n 最高気温 #{maxtemp}％\n 最低気温 #{mintemp}％\n 降水確率：\n 6〜12時 #{per06to12}％\n 12〜18時  #{per12to18}％\n 18〜24時 #{per18to24}％"
             end
           when /.*(明後日|あさって).*/
             per06to12 = doc.elements[xpath + 'info[3]/rainfallchance/period[2]l'].text
             per12to18 = doc.elements[xpath + 'info[3]/rainfallchance/period[3]l'].text
             per18to24 = doc.elements[xpath + 'info[3]/rainfallchance/period[4]l'].text
+            maxtemp = doc.elements[xpath + 'info[3]/temperature/range[1]'].text
+            mintemp = doc.elements[xpath + 'info[3]/temperature/range[2]'].text
             if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
               push =
-                "明後日の天気だよね。\n何かあるのかな？\n明後日は雨が降りそう…\n当日の朝に雨が降りそうだったら教えるからね！"
+                "明後日は雨が降るかも...\nでも、まだまだわからない！\n 最高気温 #{maxtemp}％\n 最低気温 #{mintemp}％\n 降水確率：\n 6〜12時 #{per06to12}％\n 12〜18時  #{per12to18}％\n 18〜24時 #{per18to24}％\n"
             else
               push =
-                "明後日の天気？\n気が早いねー！何かあるのかな。\n明後日は雨は降らない予定だよ(^^)\nまた当日の朝の最新の天気予報で雨が降りそうだったら教えるからね！"
+                "明後日は釣り日和になるよ！\n楽しみだね！\n 最高気温 #{maxtemp}％\n 最低気温 #{mintemp}％\n 降水確率：\n 6〜12時 #{per06to12}％\n 12〜18時  #{per12to18}％\n 18〜24時 #{per18to24}％\n"
             end
           when /.*(かわいい|可愛い|カワイイ|きれい|綺麗|キレイ|素敵|ステキ|すてき|面白い|おもしろい|ありがと|すごい|スゴイ|スゴい|好き|頑張|がんば|ガンバ).*/
             push =
-              "ありがとう！！！\n優しい言葉をかけてくれるあなたはとても素敵です(^^)"
+              "ありがとうございます！！！\n優しい言葉をかけてくれるあなたはとても素敵です！！"
           when /.*(こんにちは|こんばんは|初めまして|はじめまして|おはよう).*/
             push =
-              "こんにちは。\n声をかけてくれてありがとう\n今日があなたにとっていい日になりますように(^^)"
+              "こんにちは！\n今日は釣りですか？"
+          when /.*(占い|うらない|).*/
+            push =
+            ["大吉！！!\n爆釣の予感！！！",
+             "中吉！!\n楽しい釣りになりそう！",
+             "小吉！\n厳しい中の価値ある１本があるかも",
+             "凶...\n釣れないかも...\n根がかりに気をつけましょう！"].sample
           else
             per06to12 = doc.elements[xpath + 'info/rainfallchance/period[2]l'].text
             per12to18 = doc.elements[xpath + 'info/rainfallchance/period[3]l'].text
             per18to24 = doc.elements[xpath + 'info/rainfallchance/period[4]l'].text
+            maxtemp = doc.elements[xpath + 'info/temperature/range[1]'].text
+            mintemp = doc.elements[xpath + 'info/temperature/range[2]'].text
             if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
-              word =
-                ["雨だけど元気出していこうね！",
-                 "雨に負けずファイト！！",
-                 "雨だけどああたの明るさでみんなを元気にしてあげて(^^)"].sample
               push =
-                "今日の天気？\n今日は雨が降りそうだから傘があった方が安心だよ。\n　  6〜12時　#{per06to12}％\n　12〜18時　 #{per12to18}％\n　18〜24時　#{per18to24}％\n#{word}"
+              "雨だね...\n足元に気をつけて頑張ってね！\n 最高気温 #{maxtemp}％\n 最低気温 #{mintemp}％\n 降水確率：\n 6〜12時 #{per06to12}％\n 12〜18時  #{per12to18}％\n 18〜24時 #{per18to24}％\n"
             else
-              word =
-                ["天気もいいから一駅歩いてみるのはどう？(^^)",
-                 "今日会う人のいいところを見つけて是非その人に教えてあげて(^^)",
-                 "素晴らしい一日になりますように(^^)",
-                 "雨が降っちゃったらごめんね(><)"].sample
               push =
-                "今日の天気？\n今日は雨は降らなさそうだよ。\n#{word}"
+              "良い天気だね！\nたくさん釣れると良いね！\n 最高気温 #{maxtemp}％\n 最低気温 #{mintemp}％\n 降水確率：\n 6〜12時 #{per06to12}％\n 12〜18時  #{per12to18}％\n 18〜24時 #{per18to24}％\n"
             end
           end
           # テキスト以外（画像等）のメッセージが送られた場合
