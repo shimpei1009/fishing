5d967ae2d94f4cd61f4ed7dc80e36df99390833d
shimpei1009 <gogo.ikeike.1009@icloud.com>
2020-03-19 20:04:57 +0900

    Update linebot_controller.rb

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -23,12 +23,62 @@ class LinebotController < ApplicationController
         when Line::Bot::Event::MessageType::Text
           # event.message['text']：ユーザーから送られたメッセージ
           input = event.message['text']
-          url  = "http://fishing-community.appspot.com/tidexml/index?portid=103&year=&month=&day="
+          url  = "https://www.drk7.jp/weather/xml/13.xml"
           xml  = open( url ).read.toutf8
           doc = REXML::Document.new(xml)
+          xpath = 'weatherforecast/pref/area[4]/'
           # 当日朝のメッセージの送信の下限値は20％としているが、明日・明後日雨が降るかどうかの下限値は30％としている
+          min_per = 30
           case input
+          when /.*(今日|きょう|天気).*/
+            per00to06 = doc.elements[xpath + 'info/rainfallchance/period[1]'].text
+            per06to12 = doc.elements[xpath + 'info/rainfallchance/period[2]'].text
+            per12to18 = doc.elements[xpath + 'info/rainfallchance/period[3]'].text
+            per18to24 = doc.elements[xpath + 'info/rainfallchance/period[4]'].text
+            maxTemp = doc.elements[xpath + 'info/temperature/range[1]'].text
+            minTemp = doc.elements[xpath + 'info/temperature/range[2]'].text
+            if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
+              push =
+              "雨が降りそう...\n足元に気をつけて頑張ってね！\n\n降水確率\n0〜6時：#{per00to06}％\n6〜12時：#{per06to12}％\n12〜18時：#{per12to18}％\n18〜24時：#{per18to24}％\n\n最高気温：#{maxTemp}℃\n最低気温：#{minTemp}℃"
+            else
+              push =
+              "良い天気だね！\nたくさん釣れると良いね！\n\n降水確率\n0〜6時：#{per00to06}％\n6〜12時：#{per06to12}％\n12〜18時：#{per12to18}％\n18〜24時：#{per18to24}％\n\n最高気温：#{maxTemp}℃\n最低気温：#{minTemp}℃"
+            end
+            # 「明日」or「あした」というワードが含まれる場合
+          when /.*(明日|あした).*/
+            # info[2]：明日の天気
+            per00to06 = doc.elements[xpath + 'info[2]/rainfallchance/period[1]'].text
+            per06to12 = doc.elements[xpath + 'info[2]/rainfallchance/period[2]'].text
+            per12to18 = doc.elements[xpath + 'info[2]/rainfallchance/period[3]'].text
+            per18to24 = doc.elements[xpath + 'info[2]/rainfallchance/period[4]'].text
+            maxTemp = doc.elements[xpath + 'info[2]/temperature/range[1]'].text
+            minTemp = doc.elements[xpath + 'info[2]/temperature/range[2]'].text
+            if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
+              # もし降水確率がmin_per以上の場合
+              push = 
+              "明日は雨が降りそう...\nでも、魚は活発になるよ！\n\n降水確率\n0〜6時：#{per00to06}％\n6〜12時：#{per06to12}％\n12〜18時：#{per12to18}％\n18〜24時：#{per18to24}％\n\n最高気温：#{maxTemp}℃\n最低気温：#{minTemp}℃"
+            else
+              push = 
+              "明日は釣り日和になるよ！\nタックルの準備だ！\n\n降水確率\n0〜6時：#{per00to06}％\n6〜12時：#{per06to12}％\n12〜18時：#{per12to18}％\n18〜24時：#{per18to24}％\n\n最高気温：#{maxTemp}℃\n最低気温：#{minTemp}℃"
+            end
+          when /.*(明後日|あさって).*/
+            per00to06 = doc.elements[xpath + 'info[3]/rainfallchance/period[1]'].text
+            per06to12 = doc.elements[xpath + 'info[3]/rainfallchance/period[2]'].text
+            per12to18 = doc.elements[xpath + 'info[3]/rainfallchance/period[3]'].text
+            per18to24 = doc.elements[xpath + 'info[3]/rainfallchance/period[4]'].text
+            maxTemp = doc.elements[xpath + 'info[3]/temperature/range[1]'].text
+            minTemp = doc.elements[xpath + 'info[3]/temperature/range[2]'].text
+            if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
+              push = 
+              "明後日は雨が降るかも...\nでも、まだまだわからない！\n\n降水確率\n0〜6時：#{per00to06}％\n6〜12時：#{per06to12}％\n12〜18時：#{per12to18}％\n18〜24時：#{per18to24}％\n\n最高気温：#{maxTemp}℃\n最低気温：#{minTemp}℃"
+            else
+              push = 
+              "明後日は釣り日和になるよ！\n楽しみだね！\n\n降水確率\n0〜6時：#{per00to06}％\n6〜12時：#{per06to12}％\n12〜18時：#{per12to18}％\n18〜24時：#{per18to24}％\n\n最高気温：#{maxTemp}℃\n最低気温：#{minTemp}℃"
+            end
           when /.*(潮位|タイド|潮).*/
+            url  = "http://fishing-community.appspot.com/tidexml/index?portid=103&year=&month=&day="
+            xml  = open( url ).read.toutf8
+            doc = REXML::Document.new(xml)
             tideName = doc.elements['tideinfo/tide-name'].text
             tideTime1 = doc.elements['tideinfo/tidedetails[1]/tide-time'].text
             tideTime2 = doc.elements['tideinfo/tidedetails[2]/tide-time'].text
@@ -88,3 +138,4 @@ class LinebotController < ApplicationController
     }
   end
 end
+
