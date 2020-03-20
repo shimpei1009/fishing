c6d87ead111ff96a3c9ad4d7602bbb421a2a308c
shimpei1009 <gogo.ikeike.1009@icloud.com>
2020-03-20 17:21:40 +0900

    Update linebot_controller.rb

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -26,9 +26,9 @@ class LinebotController < ApplicationController
           xml  = open( url ).read.toutf8
           doc = REXML::Document.new(xml)
           doc.elements.each('weatherdata/forecast/time') do |time|
-          now = symbol.attributes['name'].text
-          nowTemp = temperature.attributes['value'].text
-          end
+          xpath = 'weatherdata/forecast/time/'
+          now = doc.elements[xpath + 'symbol'].attributes['name'].text
+          nowTemp = doc.elements[xpath + 'temperature'].attributes['value'].text
           if now == "clear sky" || "few clouds"
             push = "現在地の天気は#{now}です\u{2600}\n\n現在の気温は#{nowTemp}℃です\u{1F321}"
           elsif now == "scattered clouds" || "broken clouds"
